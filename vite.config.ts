a9f29a85bf62053609b4d8b99b5d55f8ec3d181c
rxliuli <rxliuli@gmail.com>
2023-06-28 21:27:41 +0800

    feat: 更好的处理 chrome/firefox 配置冲突的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,60 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 import { i18nextDtsGen } from '@liuli-util/rollup-plugin-i18next-dts-gen'
+import { cp, readFile, writeFile, rm, access } from 'fs/promises'
+import path from 'path'
+
+function buildFirefox(): Plugin {
+  let mode: string
+  return {
+    name: 'build-firefox',
+    enforce: 'post',
+    config(_, env) {
+      mode = env.mode
+    },
+    async closeBundle() {
+      if (mode !== 'build') {
+        return
+      }
+      const distFirefox = path.resolve('dist-firefox')
+      if (
+        await access(distFirefox)
+          .then(() => true)
+          .catch(() => false)
+      ) {
+        await rm(distFirefox, {
+          recursive: true,
+          force: true,
+        })
+      }
+      await cp(path.resolve('dist'), distFirefox, {
+        recursive: true,
+        force: true,
+      })
+      const json = JSON.parse(
+        await readFile(path.resolve(distFirefox, 'manifest.json'), 'utf-8'),
+      )
+      await writeFile(
+        path.resolve(distFirefox, 'manifest.json'),
+        JSON.stringify(
+          {
+            ...json,
+            browser_specific_settings: {
+              gecko: {
+                id: 'clean-twttier@rxliuli.com',
+                strict_min_version: '109.0',
+              },
+            },
+          },
+          null,
+          2,
+        ),
+      )
+    },
+  }
+}
 
 export default defineConfig({
   plugins: [
@@ -11,6 +63,7 @@ export default defineConfig({
     i18nextDtsGen({
       dirs: ['src/i18n'],
     }) as any,
+    buildFirefox(),
   ],
   base: './',
   build: {
