ff3dcfc67f6d2363e0ea81afa11186a7016302ab
rxliuli <rxliuli@gmail.com>
2023-10-20 18:38:29 +0900

    fix: 修复对性能造成的严重影响

--- src/content-script/plugins/hideOther.ts
+++ src/content-script/plugins/hideOther.ts
@@ -38,23 +38,23 @@ export function hideOther(): BasePlugin {
         ),
       )
     },
-    observer() {
-      const tag = 'whoToFollow'
-      if (!location.pathname.includes('/home')) {
-        cleanCSS(tag)
-        return
-      }
-      addCSS(
-        generateHideCSS(
-          // Who to follow
-          '[data-testid="cellInnerDiv"]:has(h2 > div > span)',
-          '[data-testid="cellInnerDiv"]:has(h2 > div > span) + *',
-          '[data-testid="cellInnerDiv"]:has(h2 > div > span) + * + *',
-          '[data-testid="cellInnerDiv"]:has(h2 > div > span) + * + * + *',
-          '[data-testid="cellInnerDiv"]:has(h2 > div > span) + * + * + * + *',
-        ),
-        tag,
-      )
-    },
+    // observer() {
+    //   const tag = 'whoToFollow'
+    //   if (!location.pathname.includes('/home')) {
+    //     cleanCSS(tag)
+    //     return
+    //   }
+    //   addCSS(
+    //     generateHideCSS(
+    //       // Who to follow
+    //       '[data-testid="cellInnerDiv"]:has(h2 > div > span)',
+    //       '[data-testid="cellInnerDiv"]:has(h2 > div > span) + *',
+    //       '[data-testid="cellInnerDiv"]:has(h2 > div > span) + * + *',
+    //       '[data-testid="cellInnerDiv"]:has(h2 > div > span) + * + * + *',
+    //       '[data-testid="cellInnerDiv"]:has(h2 > div > span) + * + * + * + *',
+    //     ),
+    //     tag,
+    //   )
+    // },
   }
 }
