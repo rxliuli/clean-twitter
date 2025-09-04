9654cfd9413a7d02b3f02e1c7d7644be7cddfe4a
rxliuli <rxliuli@gmail.com>
2025-09-04 21:00:43 +0800

    feat: upgrade deps

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'media',
-  content: ['./entrypoints/popup/index.html', './entrypoints/**/*.tsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  vite: () => ({
+    plugins: [tailwindcss()] as any,
+    resolve: {
+      alias: {
+        '@': __dirname,
+      },
+    },
+  }),
   modules: ['@wxt-dev/module-react'],
-  runner: {
+  webExt: {
     disabled: true,
   },
   manifest: {
